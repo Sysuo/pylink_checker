875dc0983251eb57b83f1d0c67a811bd7d37f22f
a-kurosawa <a-kurosawa@mi-labo.co.jp>
2019-01-04 11:15:25 +0900

    feat: コメント行を飛ばす処理を追加 ＆ スペースをタブに変換

--- check.py
+++ check.py
@@ -2,28 +2,28 @@ import requests
 from pprint import pprint
 
 def readFile(file_name):
-    data = ''
-    try:
-        file = open(file_name)
-        data = file.readlines()
-        print(data)
-    except Exception as e:
-        print("[Error](", readFile.__name__, ")", e)
-    finally:
-        if 'file' in locals():
-            file.close()
-    return data
+	data = ''
+	try:
+		file = open(file_name)
+		data = file.readlines()
+		print(data)
+	except Exception as e:
+		print("[Error](", readFile.__name__, ")", e)
+	finally:
+		if 'file' in locals():
+			file.close()
+	return data
 
 def check(url):
-    if url == "":
-        return;
-    print("[check]", url)
-    try:
-        r = requests.get(url)
-        if r.status_code != 200:
-          print("[Access Error][URL]", url, " [response code]", r.status_code)
-    except Exception as e:
-        print("[Error]", e)
+	if url == "":
+		return;
+	print("[Checking...]", url)
+	try:
+		r = requests.get(url)
+		if r.status_code != 200:
+		  print("[Access Error][URL]", url, " [response code]", r.status_code)
+	except Exception as e:
+		print("[Error]", e)
 
 
 # 読込むファイルのパス
@@ -35,6 +35,9 @@ list = readFile(file_name)
 #pprint(list)
 
 for url in list:
-    url = url.replace("\n", "")
-    url = url.replace("\r", "")
-    check(url)
+	url = url.replace("\n", "")
+	url = url.replace("\r", "")
+	# コメント行は飛ばす
+	if url.startswith('//') || url.startswith('#')':
+		continue
+	check(url)
